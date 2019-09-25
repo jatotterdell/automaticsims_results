163b03981d89aca1bc32b129c4748cd432b9f7a2
James Totterdell <jatotterdell@gmail.com>
2019-09-26 06:51:36 +0800

    Add scenario

--- noninferiority_sce5.R
+++ noninferiority_sce5.R
@@ -4,70 +4,76 @@ library(tidyverse)
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
       j, rep(1, 13), 0.1, 
-      kappa_hi_0 = 0.9, kappa_hi_1 = 0.9,
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_null.rds")
+saveRDS(resall, "out/noninferior/sce5_null.rds")
 
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
       j, c(1, 1.5, rep(1, 11)), 0.1, 
-      kappa_hi_0 = 0.9, kappa_hi_1 = 0.9, 
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_one_sup.rds")
+saveRDS(resall, "out/noninferior/sce5_one_sup.rds")
 
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
       j, c(1, 1.5, 1.5, 1.5, rep(1, 9)), 0.1, 
-      kappa_hi_0 = 0.9, kappa_hi_1 = 0.9, 
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_three_sup.rds")
-
+saveRDS(resall, "out/noninferior/sce5_three_sup.rds")
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
       j, c(1, rep(1.5, 3), rep(1.25, 3), rep(1, 6)), 0.1, 
-      kappa_hi_0 = 0.9, kappa_hi_1 = 0.9, 
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_two_msg_benefit.rds")
+saveRDS(resall, "out/noninferior/sce5_two_msg_benefit.rds")
 
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
-      j, seq(1,1.5, length.out = 13), 0.1, kappa_hi_0 = 0.9, kappa_hi_1 = 0.9, 
+      j, seq(1,1.5, length.out = 13), 0.1, 
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_increase_seq.rds")
+saveRDS(resall, "out/noninferior/sce5_increase_seq.rds")
 
 
 st <- system.time(
   res <- lapply(
-    1:10000, 
+    1:1000, 
     function(j) run_a_noninf_trial(
-      j, c(1.25, rep(1, 12)), 0.1, kappa_hi_0 = 0.9, kappa_hi_1 = 0.9,
+      j, c(1.25, rep(1, 12)), 0.1, 
+      kappa_hi_0 = 0.85, kappa_hi_1 = 0.85, 
+      kappa_lo_0 = 0.01, kappa_lo_1 = 0.01, 
       return_all = F, allocate_inactive = F, brar = T))
 )
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
-saveRDS(resall, "out/noninferior/sce3_ctrl_best.rds")
-
+saveRDS(resall, "out/noninferior/sce5_ctrl_best.rds")
