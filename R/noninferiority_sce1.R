e20dcec269c8cdd327e93fe108e9a9c28deb5f84
James Totterdell <jatotterdell@gmail.com>
2019-09-25 13:20:18 +0800

    Update

--- R/noninferiority_sce1.R
+++ R/noninferiority_sce1.R
@@ -44,33 +44,43 @@ st <- system.time(
 resall <- as_tibble(do.call(rbind, map(res, simplify)))
 saveRDS(resall, "out/noninferior/sce1_two_msg_benefit.rds")
 
+st <- system.time(
+  res <- lapply(
+    1:10000, 
+    function(j) run_a_noninf_trial(
+      j, seq(1,1.5, length.out = 13), 0.1, 
+      return_all = F, allocate_inactive = F, brar = T))
+)
+resall <- as_tibble(do.call(rbind, map(res, simplify)))
+saveRDS(resall, "out/noninferior/sce1_increase_seq.rds")
 
 
-
-resall0 %>% select("interim", "superior", "noninferior", starts_with("n.")) %>% summarise_all(mean)
-resall0 %>% 
-  select(starts_with("p_sup")) %>% 
-  summarise_all(function(x) mean(x > 0.9)) %>% 
-  matrix(13,13,byrow=T)
-resall0 %>% 
-  filter(superior == 1) %>%
-  select(starts_with("p_sup")) %>% 
-  summarise_all(function(x) mean(x > 0.9)) %>% 
-  matrix(13,13,byrow=T)
-
-resall0 %>% 
-  filter(noninferior == 1) %>%
-  select(starts_with("p_sup")) %>% 
-  summarise_all(function(x) mean(x > 0.9)) %>% 
-  matrix(13,13,byrow=T)
-# When stopping for non-inferiority, only 1-3 is ever still active
-resall0 %>% 
-  filter(noninferior == 1) %>%
-  select(starts_with("active")) %>% 
-  summarise_all(mean)
-resall0 %>% 
-  filter(noninferior == 1) %>%
-  select(starts_with("active")) %>% 
-  mutate(`Total noninferior` = rowSums(.)) %>%
-  count(`Total noninferior`) %>%
-  mutate(p = n / sum(n))
+# 
+# 
+# resall0 %>% select("interim", "superior", "noninferior", starts_with("n.")) %>% summarise_all(mean)
+# resall0 %>% 
+#   select(starts_with("p_sup")) %>% 
+#   summarise_all(function(x) mean(x > 0.9)) %>% 
+#   matrix(13,13,byrow=T)
+# resall0 %>% 
+#   filter(superior == 1) %>%
+#   select(starts_with("p_sup")) %>% 
+#   summarise_all(function(x) mean(x > 0.9)) %>% 
+#   matrix(13,13,byrow=T)
+# 
+# resall0 %>% 
+#   filter(noninferior == 1) %>%
+#   select(starts_with("p_sup")) %>% 
+#   summarise_all(function(x) mean(x > 0.9)) %>% 
+#   matrix(13,13,byrow=T)
+# # When stopping for non-inferiority, only 1-3 is ever still active
+# resall0 %>% 
+#   filter(noninferior == 1) %>%
+#   select(starts_with("active")) %>% 
+#   summarise_all(mean)
+# resall0 %>% 
+#   filter(noninferior == 1) %>%
+#   select(starts_with("active")) %>% 
+#   mutate(`Total noninferior` = rowSums(.)) %>%
+#   count(`Total noninferior`) %>%
+#   mutate(p = n / sum(n))
