4cf90c27c9a2b66f322f9c9c2a40c337f33b7650
James Totterdell <jatotterdell@gmail.com>
2019-10-14 15:53:54 +0800

    Add another mu

--- R/noninf_sims1.R
+++ R/noninf_sims1.R
@@ -54,4 +54,28 @@ for(a in 1:nrow(pars)) {
 res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
 rm(list = paste0("res", 1:nrow(pars)))
 
-saveRDS(res, "out/noninferior/mu_null.rds")
+saveRDS(res, "out/noninferior/mu_null1.rds")
+
+
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a), 
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:1000,     
+             function(j) run_a_noninf_trial(
+               j, rep(0, 13), pars$delta[a], 
+               kappa_lo_0 = pars$kappa_lo[a], kappa_lo_1 = pars$kappa_lo[a],
+               kappa_hi_0 = pars$kappa_hi[a], kappa_hi_1 = pars$kappa_hi[a],
+               kappa_no_0 = pars$kappa_no[a], kappa_no_1 = pars$kappa_no[a],
+               ctrl_alloc = pars$alloc_ctrl[a],
+               return_all = F, allocate_inactive = F, brar = T),
+             mc.cores = parallel::detectCores() - 1), 
+           simplify))))
+}
+
+
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+
+saveRDS(res, "out/noninferior/mu_null2.rds")
