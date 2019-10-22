47e2bdff08764916302a621aa9f28a17e84c3079
James Totterdell <jatotterdell@gmail.com>
2019-10-22 12:44:09 +0800

    Update

--- R/nonsup_sim2.R
+++ R/nonsup_sim2.R
@@ -21,6 +21,31 @@ for(a in 1:nrow(pars)) {
            mclapply(
              1:1000,
              function(j) run_a_noninf_trial(
+               j, c(1, rep(1.1, 2), rep(1, 10)), 
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_noninf[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup[a], kappa_sup_1 = pars$kappa_sup[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup[a], kappa_nonsup_1 = pars$kappa_nonsup[a],
+               ctrl_alloc = pars$alloc_ctrl[a],
+               return_all = F, allocate_inactive = F, brar = T),
+             mc.cores = parallel::detectCores() - 1),
+           simplify))))
+}
+
+
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+saveRDS(res, "out/nonsuperiority/mu_two_sup0.rds")
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a),
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:1000,
+             function(j) run_a_noninf_trial(
                j, c(1, rep(1.25, 2), rep(1, 10)), 
                delta_sup = pars$delta_sup[a],
                delta_noninf = pars$delta_noninf[a],
