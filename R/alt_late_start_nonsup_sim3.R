85df4cecb7ce9be3b0ca1935c8465757d0693d12
James Totterdell <jatotterdell@gmail.com>
2019-10-29 11:14:15 +0800

    Add late start interim scenario

---
+++ R/alt_late_start_nonsup_sim3.R
@@ -0,0 +1,94 @@
+library(parallel)
+library(automaticsims)
+library(tidyverse)
+
+pars <- expand.grid(
+  delta_sup = c(0.05, 0.1),
+  delta_noninf = 0.1,
+  kappa_act = c(0.01, 0.005),
+  kappa_sup_0 = c(0.75, 0.85),
+  kappa_sup_1 = 0.75,
+  kappa_ctr = 0.95,
+  kappa_nonsup = c(0.05, 0.1),
+  kappa_noninf = c(0.5, 0.6),
+  alloc_ctrl = 1/13
+)
+pars$pars <- factor(as.character(1:nrow(pars)), levels = 1:nrow(pars))
+
+n_sup <- 3
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a),
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:1000,
+             function(j) run_a_noninf_trial_alt(
+               j, c(1, rep(1.1, n_sup), rep(1, 12 - n_sup)),
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_noninf[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup[a], kappa_nonsup_1 = pars$kappa_nonsup[a],
+               ctrl_alloc = pars$alloc_ctrl[a],
+               return_all = F, allocate_inactive = F, brar = T),
+             mc.cores = parallel::detectCores() - 1),
+           simplify))))
+}
+
+
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+saveRDS(res, "out/nonsuperiority/alt_late_start_mu_three_sup0.rds")
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a),
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:1000,
+             function(j) run_a_noninf_trial_alt(
+               j, c(1, rep(1.25, n_sup), rep(1, 12 - n_sup)),
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_noninf[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup[a], kappa_nonsup_1 = pars$kappa_nonsup[a],
+               ctrl_alloc = pars$alloc_ctrl[a],
+               return_all = F, allocate_inactive = F, brar = T, Nseq = seq(2500, 10000, 500)),
+             mc.cores = parallel::detectCores() - 1),
+           simplify))))
+}
+
+
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+saveRDS(res, "out/nonsuperiority/alt_late_start_mu_three_sup1.rds")
+
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a),
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:1000,
+             function(j) run_a_noninf_trial_alt(
+               j, c(1, rep(1.5, n_sup), rep(1, 12 - n_sup)),
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_noninf[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup[a], kappa_nonsup_1 = pars$kappa_nonsup[a],
+               ctrl_alloc = pars$alloc_ctrl[a],
+               return_all = F, allocate_inactive = F, brar = T, Nseq = seq(2500, 10000, 500)),
+             mc.cores = parallel::detectCores() - 1),
+           simplify))))
+}
+
+
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+saveRDS(res, "out/nonsuperiority/alt_late_start_mu_three_sup2.rds")
