82cdeaa00140300b12c2dd191f41840250182a7d
James Totterdell <jatotterdell@gmail.com>
2019-11-19 11:36:35 +0800

    Add scenarios to final sims.

--- R/final_nonsup_sim3.R
+++ R/final_nonsup_sim3.R
@@ -3,7 +3,7 @@ library(automaticsims)
 library(tidyverse)
 
 nsims <- 10000
-nsup <- 0
+nsup <- 3
 mu1 <- 1.25
 mu2 <- 1.5
 
@@ -27,7 +27,7 @@ for(a in 1:nrow(pars)) {
            mclapply(
              1:nsims,
              function(j) run_a_noninf_trial_alt(
-               j, c(rep(0, 13 - nsup), rep(mu1, nsup)), 
+               j, c(1, rep(mu1, nsup), rep(1, 12 - nsup)), 
                delta_sup = pars$delta_sup[a],
                delta_noninf = pars$delta_sup[a],
                kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
@@ -43,29 +43,29 @@ for(a in 1:nrow(pars)) {
 }
 res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
 rm(list = paste0("res", 1:nrow(pars)))
-saveRDS(res, "out/nonsuperiority/final_mu_none_sup1.rds")
+saveRDS(res, "out/nonsuperiority/final_mu_three_sup1.rds")
 
 
 for(a in 1:nrow(pars)) {
   assign(paste0("res", a),
-       as_tibble(do.call(rbind, map(
-         mclapply(
-           1:nsims,
-           function(j) run_a_noninf_trial_alt(
-             j, c(rep(1, 13 - nsup), rep(mu2, nsup)), 
-             delta_sup = pars$delta_sup[a],
-             delta_noninf = pars$delta_sup[a],
-             kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
-             kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
-             kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
-             kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
-             kappa_nonsup_0 = pars$kappa_nonsup_0[a], kappa_nonsup_1 = pars$kappa_nonsup_1[a],
-             Nseq = seq(pars$Nfirst[a], 10000, 500),
-             ctrl_alloc = pars$alloc_ctrl[a], ind_comp_ctrl = T,
-             return_all = F, allocate_inactive = F, brar = T),
-           mc.cores = parallel::detectCores()),
-         simplify))))
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:nsims,
+             function(j) run_a_noninf_trial_alt(
+               j, c(1, rep(mu2, nsup), rep(1, 12 - nsup)), 
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_sup[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup_0[a], kappa_nonsup_1 = pars$kappa_nonsup_1[a],
+               Nseq = seq(pars$Nfirst[a], 10000, 500),
+               ctrl_alloc = pars$alloc_ctrl[a], ind_comp_ctrl = T,
+               return_all = F, allocate_inactive = F, brar = T),
+             mc.cores = parallel::detectCores()),
+           simplify))))
 }
 res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
 rm(list = paste0("res", 1:nrow(pars)))
-saveRDS(res, "out/nonsuperiority/final_mu_none_sup2.rds")
+saveRDS(res, "out/nonsuperiority/final_mu_three_sup2.rds")
