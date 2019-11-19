a4457e3f35a1297612a60fefc76ae328996c90f4
James Totterdell <jatotterdell@gmail.com>
2019-11-19 12:58:58 +0800

    Add another effect size.

--- R/final_nonsup_sim2.R
+++ R/final_nonsup_sim2.R
@@ -6,6 +6,7 @@ nsims <- 10000
 nsup <- 2
 mu1 <- 1.25
 mu2 <- 1.5
+mu3 <- 1.1
 
 pars <- expand.grid(
   delta_sup = 0.1,
@@ -69,3 +70,27 @@ for(a in 1:nrow(pars)) {
 res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
 rm(list = paste0("res", 1:nrow(pars)))
 saveRDS(res, "out/nonsuperiority/final_mu_two_sup2.rds")
+
+for(a in 1:nrow(pars)) {
+  assign(paste0("res", a),
+         as_tibble(do.call(rbind, map(
+           mclapply(
+             1:nsims,
+             function(j) run_a_noninf_trial_alt(
+               j, c(1, rep(mu3, nsup), rep(1, 12 - nsup)), 
+               delta_sup = pars$delta_sup[a],
+               delta_noninf = pars$delta_sup[a],
+               kappa_act_0 = pars$kappa_act[a], kappa_act_1 = pars$kappa_act[a],
+               kappa_sup_0 = pars$kappa_sup_0[a], kappa_sup_1 = pars$kappa_sup_1[a],
+               kappa_ctr_0 = pars$kappa_ctr[a], kappa_ctr_1 = pars$kappa_ctr[a],
+               kappa_noninf_0 = pars$kappa_noninf[a], kappa_noninf_1 = pars$kappa_noninf[a],
+               kappa_nonsup_0 = pars$kappa_nonsup_0[a], kappa_nonsup_1 = pars$kappa_nonsup_1[a],
+               Nseq = seq(pars$Nfirst[a], 10000, 500),
+               ctrl_alloc = pars$alloc_ctrl[a], ind_comp_ctrl = T,
+               return_all = F, allocate_inactive = F, brar = T),
+             mc.cores = parallel::detectCores()),
+           simplify))))
+}
+res <- bind_rows(lapply(1:nrow(pars), function(x) get(paste0("res", x))), .id = "pars")
+rm(list = paste0("res", 1:nrow(pars)))
+saveRDS(res, "out/nonsuperiority/final_mu_two_sup3.rds")
